b9f70f20aece65a90826880b329098a31041ef4e
DevyanaCT <chandthakuridevyana@gmail.com>
2025-09-30 12:27:42 +0900

    Why is it so hard

--- src/rpn.py
+++ src/rpn.py
@@ -1,5 +1,32 @@
 
 
 def eval_rpn(tokens):
-    # TODO: implement RPN evaluation using a stack
-    raise NotImplementedError
+    stack = []
+
+    for token in tokens:
+        if token in {"+", "-", "*", "/"}:
+            # Pop the last two numbers from the stack
+            b = stack.pop()
+            a = stack.pop()
+
+            # Perform the operation
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            elif token == "/":
+                # Python division needs truncation toward zero
+                stack.append(int(a / b))
+        else:
+            # Token is a number, push to stack
+            stack.append(int(token))
+
+    # The final result will be the only element left in the stack
+    return stack[0]
+
+# Example usage:
+print(eval_rpn(["2","1","+","3","*"]))  # Output: 9
+print(eval_rpn(["4","13","5","/","+"])) # Output: 6
+
